d9faa95680adeadf6340662de8faab0bd1d3de38
Chonny <mama.whowho@gmail.com>
2020-08-13 14:29:19 +0800

    docs(error.dto): add comment

--- src/shared/models/error.dto.ts
+++ src/shared/models/error.dto.ts
@@ -1,5 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger'
 
+/**
+ * 發生 400, 401, 500 等錯誤時回傳的 JSON 格式
+ *
+ * ```
+ * {
+ *   statusCode: 400,
+ *   message: string[] | string,
+ *   error: 'Bad Request' | 'Internal Server Error' ...
+ * }
+ * ```
+ */
 export default class ErrorResponseDto {
   @ApiProperty({ description: '錯誤類型代碼', example: 400 })
   public statusCode: number
