f1d061f1f4620544323813ef8657f11340e0a996
Chonny <mama.whowho@gmail.com>
2020-08-17 15:43:04 +0800

    docs: add comment for transform.interceptor behavior

--- src/shared/interceptors/transform.interceptor.ts
+++ src/shared/interceptors/transform.interceptor.ts
@@ -11,13 +11,33 @@ export interface Response<T> {
   data: T
 }
 
+/**
+ * 將回傳的 JSON 轉成有 data 包覆的格式
+ *
+ * 單物件資料，原本是
+ * ```
+ * { name: 'Chonny' }
+ * ```
+ * 會被轉為
+ * ```
+ * { data: { name: 'Chonny' } }
+ * ```
+ *
+ * 清單資料
+ * ```
+ * {
+ *   data: {
+ *     data: [],
+ *     total: 0,
+ *     page: 1,
+ *     pageSize: 10
+ *   }
+ * }
+ * ```
+ */
 @Injectable()
-export class TransformInterceptor<T>
-  implements NestInterceptor<T, Response<T>> {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Observable<Response<T>> {
+export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
     return next.handle().pipe(map((data) => ({ data })))
   }
 }
